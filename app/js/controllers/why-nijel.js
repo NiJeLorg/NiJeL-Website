952d0147095557ae4eea2a8ed4417a9450e3d51f
Joseph Bosire <kashboss@gmail.com>
2017-11-13 19:32:48 +0300

    Bug fix for linking between menu and tabs
    This fixes the bug where the menu for the why-nijel section did not navigate to the appropriate tab
    
    Signed-off-by: Joseph Bosire <kashboss@gmail.com>

--- app/js/controllers/why-nijel.js
+++ app/js/controllers/why-nijel.js
@@ -1,14 +1,17 @@
-const WhyNijelCtrl = function ($scope, ClientDataService, $sce) {
+const WhyNijelCtrl = function ($scope, ClientDataService, $sce, $stateParams) {
     $scope.trustAsHtml = (template) => {
         return $sce.trustAsHtml(template);
     };
-    
+    $scope.currentUrlTab = $stateParams.tab;
     ClientDataService.fetchWhyNijelSections()
         .then((resp) => {
             $scope.sections = resp.data.sections;
         }, (err) => {
             console.error(err, 'ERROR');
-        });
+        });   
+    $scope.isActive = (tab) => {
+        return $scope.currentUrlTab == tab;
+    }
 };
 
 export default WhyNijelCtrl;
